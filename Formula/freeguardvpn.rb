503edc66039209b2d56cff3272c867f2bf69bcda
adamliu <jojohuang9209@gmail.com>
2026-03-18 10:01:01 -0700

    bump freeguardvpn to v0.2.0 (bundled deps)

--- Formula/freeguardvpn.rb
+++ Formula/freeguardvpn.rb
@@ -4,26 +4,26 @@
 class Freeguardvpn < Formula
   desc "Command-line VPN client powered by mihomo core"
   homepage "https://freeguardvpn.com"
-  version "0.1.1"
+  version "0.2.0"
   license :cannot_represent
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://downloadcli.freeguardvpn.com/cli/v#{version}/freeguard-cli_#{version}_darwin_arm64.tar.gz"
-      sha256 "1e72e776b96b5f7385e0c6d55cd781a61c89f091283c50dc213035137b7c6bfb"
+      sha256 "f2dc8ac80345cd8bdacc0b15f9f74d7888332a44b664eb5e0d5632dfa69c375a"
     else
       url "https://downloadcli.freeguardvpn.com/cli/v#{version}/freeguard-cli_#{version}_darwin_amd64.tar.gz"
-      sha256 "b020600881e3027f08fdf2a3bddcb76b6a57f0c3ef3144f70da2df02d85036a6"
+      sha256 "75478360c5b7232a53fa52ea3b21f8868518b7316cd4214efc3eeb1edba222c0"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://downloadcli.freeguardvpn.com/cli/v#{version}/freeguard-cli_#{version}_linux_arm64.tar.gz"
-      sha256 "d7ea26d05fcee2d693c31bf590072ac4e37adb67f4f8c9bff7f4b6b6a0b20990"
+      sha256 "c835a5add4a106af4a194ca68037af7ea04dabeee8014999da65858c6e08f1e9"
     else
       url "https://downloadcli.freeguardvpn.com/cli/v#{version}/freeguard-cli_#{version}_linux_amd64.tar.gz"
-      sha256 "daa4b7987550ba5ab6c66512b83eeca5abe347c27fc401664ae57a7b5b8fcc59"
+      sha256 "f0768a44555c5a95e4b23de07a962c4fe508e4149918d998cf70ce91b4302c9e"
     end
   end
 
